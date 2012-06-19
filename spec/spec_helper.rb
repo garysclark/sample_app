9ac1e55062d700915ef9e15bef00e818802957e8
Gary S. Clark <garysclark@yahoo.com>
2012-06-18 19:01:37 -0700

    Add automated tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -30,3 +30,10 @@ RSpec.configure do |config|
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
 end
+
+module ::RSpec::Core
+  class ExampleGroup
+    include Capybara::DSL
+    include Capybara::RSpecMatchers
+  end
+end
